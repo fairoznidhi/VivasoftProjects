4b17863d58df1644ecf4ae273f758d16dbbaf6ec
fairoznidhi <fairoz1697@gmail.com>
2025-07-18 19:05:30 +0600

    18/7/25

---
+++ career-pilot/career-pilot-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-7xl bg-">Hello</h1>
+      <Outlet/>
+    </div>
+  );
+}
+
+export default App;
